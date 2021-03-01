24fc224ff3a6ffaca76e12154405e7c82615dc99
cnridley <cnridley1@gmail.com>
2021-03-01 04:52:28 +0000

    set up models to upload pdfs and rendered on to djangopages.html

--- djangopage/models.py
+++ djangopage/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class PdfUpload(models.Model):
+    title = models.CharField(max_length=254, null=True, blank=True)
+    fileUpload = models.ImageField(upload_to='media/', blank=True)
+
+    def __str__(self):
+        return self.title
--- djangopage/views.py
+++ djangopage/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from .models import PdfUpload
 
 # Create your views here.
 
 def djangopage(request):
     """A view to return index page"""
 
-    return render(request, 'django.html')
+    Pdf = PdfUpload.objects.all()
+
+    context = {
+        'Pdf': Pdf,
+    }
+
+    return render(request, 'django.html', context)
+
